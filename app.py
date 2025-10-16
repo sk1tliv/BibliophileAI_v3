29daa814e8cd3ce318b69f059392b453e4cf4461
kıvanc <175986696+sk1tliv@users.noreply.github.com>
2025-10-16 18:47:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -237,5 +237,5 @@ def load_conversation(session_id):
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
